d5762e40b1e2e1a403e3edf0deeb7a350377db67
arun0595 <117884579+arun0595@users.noreply.github.com>
2025-10-13 18:54:32 -0400

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,13 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
+
 output "ecs_cluster_name" {
   value = aws_ecs_cluster.cluster.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.service.name
 }
 
 output "load_balancer_dns" {
@@ -8,4 +16,5 @@ output "load_balancer_dns" {
 
 output "cloudwatch_log_group" {
   value = aws_cloudwatch_log_group.ecs_log_group.name
+
 }
