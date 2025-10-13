6a47c1c1b9fe6348f9e79e6f973153cdd80310dd
arun0595 <117884579+arun0595@users.noreply.github.com>
2025-10-13 18:28:58 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-#remote backend state configuration
-terraform {
-  backend "s3" {
-    bucket = "Arun-terraform-state-bucket"
-    key    = "ecs/hello-world-app/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
 
 provider "aws" {
   region = var.region
